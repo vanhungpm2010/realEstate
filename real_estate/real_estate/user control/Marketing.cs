0a41784e8b8cdfa2ee01b259f7d4160b545a1a46
Nguyễn Văn Hùng <outking18@gmail.com>
2018-10-13 22:23:27 +0700

    add user control

---
+++ real_estate/real_estate/user control/Marketing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace real_estate.user_control
+{
+    public partial class Marketing : UserControl
+    {
+        public Marketing()
+        {
+            InitializeComponent();
+        }
+    }
+}
